44b1395e77700b64839f4a1e7fd07390fa0a80f5
João Victor <guedesjvictor@gmail.com>
2025-04-05 22:14:34 -0300

    Iniciando projeto

---
+++ src/test/java/com/unifacisa/neomed/NeomedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unifacisa.neomed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeomedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
