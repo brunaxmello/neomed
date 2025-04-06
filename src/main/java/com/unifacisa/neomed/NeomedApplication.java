13355133c406e1e55b897eda4830c8588a75ac19
João Victor <guedesjvictor@gmail.com>
2025-04-05 21:45:00 -0300

    Iniciando Projeto Neomed

---
+++ src/main/java/com/unifacisa/neomed/NeomedApplication.java
@@ -0,0 +1,13 @@
+package com.unifacisa.neomed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeomedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeomedApplication.class, args);
+	}
+
+}
