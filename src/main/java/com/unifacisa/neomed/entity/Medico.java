77949b6722bed2dfc66933ddea1e5c9dbeb8af3d
Cauany Nunes Rodrigues <cauanyrodrigues01@gmail.com>
2025-04-07 07:55:19 -0300

    adicionado demias tags do lombok

--- src/main/java/com/unifacisa/neomed/entity/Medico.java
+++ src/main/java/com/unifacisa/neomed/entity/Medico.java
@@ -2,19 +2,27 @@ package com.unifacisa.neomed.entity;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.unifacisa.neomed.Avaliacao;
 import com.unifacisa.neomed.Localizacao;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(callSuper = true)
 public class Medico extends Usuario {
 	
 	@Getter @Setter
