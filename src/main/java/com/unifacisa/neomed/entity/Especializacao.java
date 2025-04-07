e28c29bc246598e9ebdcbfde0ff9134b3e7ef374
jeffersonthiagofn <jeffersonthiagofn@gmail.com>
2025-04-07 09:17:49 -0300

    Atualizando a entidade especialização

--- src/main/java/com/unifacisa/neomed/entity/Especializacao.java
+++ src/main/java/com/unifacisa/neomed/entity/Especializacao.java
@@ -2,15 +2,23 @@ package com.unifacisa.neomed.entity;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
- 
+import lombok.ToString;
+
 @Entity
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(callSuper = true)
 public class Especializacao {
 
 	@Id
@@ -23,5 +31,7 @@ public class Especializacao {
 	private String nome;
 
 	@ManyToMany(mappedBy = "especializacoes")
+	@JsonBackReference
 	private List<Medico> medicos;
+
 }
