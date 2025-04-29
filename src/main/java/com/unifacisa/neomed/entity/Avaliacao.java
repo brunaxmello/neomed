684f850e8996e04bdbd7c7e674a3d507969a4190
Samuel gs <samuelgomessoares14@gmail.com>
2025-04-29 20:19:59 -0300

    Adicionando o Lombok na classe avaliação

--- src/main/java/com/unifacisa/neomed/entity/Avaliacao.java
+++ src/main/java/com/unifacisa/neomed/entity/Avaliacao.java
@@ -5,8 +5,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data @NoArgsConstructor @EqualsAndHashCode
 public class Avaliacao {
 
 	@Id
@@ -20,57 +24,8 @@ public class Avaliacao {
 	private Usuario usuario;
 	
 	@ManyToOne
-	private Medico medico;
+	private ProfissionalDeSaude profissionalDeSaude;
 
-	public Avaliacao(Long id, Integer nota, String comentario, Usuario usuario, Medico medico) {
-		super();
-		this.id = id;
-		this.nota = nota;
-		this.comentario = comentario;
-		this.usuario = usuario;
-		this.medico = medico;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Integer getNota() {
-		return nota;
-	}
-
-	public void setNota(Integer nota) {
-		this.nota = nota;
-	}
-
-	public String getComentario() {
-		return comentario;
-	}
-
-	public void setComentario(String comentario) {
-		this.comentario = comentario;
-	}
-
-	public Usuario getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(Usuario usuario) {
-		this.usuario = usuario;
-	}
-
-	public Medico getMedico() {
-		return medico;
-	}
-
-	public void setMedico(Medico medico) {
-		this.medico = medico;
-	}
-	
 	
 	
 }
