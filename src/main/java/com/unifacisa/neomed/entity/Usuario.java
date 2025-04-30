fa527231c6851d6d04b1c3c60a8a6dae55854a80
João Victor <guedesjvictor@gmail.com>
2025-04-29 22:36:07 -0300

    Adiciona restrições de coluna na entidade Usuario

--- src/main/java/com/unifacisa/neomed/entity/Usuario.java
+++ src/main/java/com/unifacisa/neomed/entity/Usuario.java
@@ -1,5 +1,6 @@
 package com.unifacisa.neomed.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,9 +20,12 @@ public class Usuario {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Column(length = 50)
 	private String nome;
-
+	
+	@Column(length = 50, unique = true)
 	private String email;
 
+	@Column(length = 50)
 	private String senha;
 }
