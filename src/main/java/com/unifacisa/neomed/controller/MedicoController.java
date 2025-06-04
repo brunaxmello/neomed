a4e3f4eb41594d4ec30df33eddbdaa88c2b2c6c7
brunaxmello <bruna.mello@maisunifacisa.com.br>
2025-06-04 19:56:42 -0300

    Metodo Get no Controller de Listar médicos

--- src/main/java/com/unifacisa/neomed/controller/MedicoController.java
+++ src/main/java/com/unifacisa/neomed/controller/MedicoController.java
@@ -1,5 +1,27 @@
 package com.unifacisa.neomed.controller;
 
-public class MedicoController {
+import java.util.List;
 
-}
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.unifacisa.neomed.entity.Medico;
+import com.unifacisa.neomed.service.MedicoService;
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+	
+	@Autowired
+	private MedicoService medicoService; 
+	
+	@GetMapping
+	public List<Medico> lisarTodos() {
+		return medicoService.listarTodos();
+	}
+	
+}	
+	
+
